7c37f71c1b56a437fd88d8bbe025fff6510d6353
Michael Bohin <mbohin@seznam.cz>
2022-06-06 14:37:29 +0200

    Place from which nice coding exercise may start...

---
+++ 01-DDDice-Statistics-Calculator/DDDice.cs
@@ -0,0 +1,32 @@
+﻿namespace DnD_Probability_Space;
+
+class DDDice_Mass_Thrower {
+	Random rand = new();
+	readonly int max, exclusiveMax, throws;
+	public readonly int minSum, maxSum;
+	public uint[] statistic;
+
+	public DDDice_Mass_Thrower(DiceType m, int t) {
+		max = (int)m;
+		exclusiveMax = max + 1;
+		throws = t;
+		minSum = t;
+		maxSum = max * throws;
+		statistic = new uint[maxSum+1];
+	}
+
+	public uint[] GenerateStatistic(uint rolls) {
+		for(int i = 0; i < rolls; i++) {
+			int sum = ThrowDice();
+			statistic[sum]++;
+		}
+		return statistic;
+	}
+
+	int ThrowDice() {
+		int rollSum = 0;
+		for(int i = 0; i < throws; i++) 
+			rollSum += rand.Next(1, exclusiveMax);
+		return rollSum;
+	}
+}
---
+++ 01-DDDice-Statistics-Calculator/Probability-space-crawler.cs
@@ -0,0 +1,63 @@
+﻿namespace DnD_Probability_Space;
+using static System.Math;
+public enum DiceType { d4 = 4, d6 = 6, d8 = 8, d10 = 10, d12 = 12, d20 = 20 };
+
+public record Partition {
+	public List<uint> parts = new();
+	public ulong compositions;
+}
+
+public record RollSum {
+	public uint sum;
+	public List<Partition> partitions = new();
+	public ulong totalCompositions;
+	public double probability; // = totalCompositions / possibleOutcomes
+}
+
+
+public class DnD_Dice_Probability_Space_Crawler {
+	//
+	// >>> these data are important for json serializer and catch all results of space crawl <<<
+	//
+	public readonly uint dices;
+	public readonly DiceType diceType;
+	public readonly uint minRollSum, maxRollSum; // defines inclusive range of possible roll outcomes
+	public readonly ulong possibleOutcomes;
+	public ulong allCompositions; // sum of totalCompositions of all roll sums, used as double check of correctness. Must be equal to possible Outcomes.
+	public List<RollSum> rollSums_data = new();
+
+	//
+	// >>> end of serializer relevant data <<<
+	//
+
+	// Factorials up to 9! are needed to calculate number of compositions.
+	// This assumes we will be working with rolls with at most 9 dices. 
+	// For ten dices calculation this would result in out of bounds exception.
+	readonly uint[] factorial = new uint[10]; 
+
+	public DnD_Dice_Probability_Space_Crawler(uint d, DiceType dt) {
+		dices = d;
+		diceType = dt;
+
+		// precalculate values of first ten factorials:
+		factorial[0] = 1;
+		for (uint i = 1; i < 10; i++) 
+			factorial[i] = factorial[i-1] * i;
+
+		minRollSum = dices;
+		uint diceMaxVal = (uint)diceType;
+		maxRollSum = dices * diceMaxVal;
+		possibleOutcomes = (ulong) Pow(diceMaxVal, dices);
+	}
+
+	public void SearchPartitions() { // and for each partition compute number of its compositions...
+		// .. write your code here ..
+
+	}
+
+	ulong Compositions(List<uint> partition) {
+		// .. write your code here ..
+
+		return 1;
+	}
+}
